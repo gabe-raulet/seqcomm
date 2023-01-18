31c5a32ed54f623fde32aa9d44403fa61c23b7da
Gabriel Raulet <gabe.h.raulet@gmail.com>
2023-01-18 15:01:11 -0800

    init

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <mpi.h>
+
+/*
+ * 1. Master process reads .fai file and parses each line into a fasta record.
+ *
+ * 2. Master process scatters FASTA records to each process in 2D grid, using
+ *    linear decomposition.
+ *
+ * 3. Each process in parallel reads in its sequences from the FASTA (using collective I/O)
+ *    and compresses the sequences into a storage buffer.
+ *
+ * 4. A collective Allgather across the rows of the 2D grid occurs with the storage buffers
+ *    on each process being exchanged.
+ *
+ * 5. Step 4 is repeated using the columns.
+ *
+ * 6. Unpack/decompress local sequences.
+ *
+ * 7. At this point, every process should have access to the sequence info it needs
+ *    in order to use CombBLAS.
+ *
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
